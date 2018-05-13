281277f1656a2c482b76a1b8abea6b7a3bff10e6
Phil <philbone@focused.cl>
2018-05-12 23:35:05 -0300

    Resuelve la inyección implicita de dependencias en SellerController.

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -27,11 +27,9 @@ class SellerController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Seller $seller)
     {
-        $vendedor = Seller::has('products')->findOrFail($id);
-
-        return $this->showOne($vendedor);
+        return $this->showOne($seller);
     }
 
 }
