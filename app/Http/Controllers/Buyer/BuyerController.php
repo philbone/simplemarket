68dfad9bcce7916f66610ac408265074f3f60e2a
Phil <philbone@focused.cl>
2018-05-12 23:24:34 -0300

    Resuelve la inyección implicita de dependencias en BuyerController mediante Global Scopes.

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -27,11 +27,9 @@ class BuyerController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Buyer $buyer)
     {
-        $comprador = Buyer::has('transactions')->findOrFail($id);
-
-        return $this->showOne($comprador);
+        return $this->showOne($buyer);
     }
 
 
