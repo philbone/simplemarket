d769e6e2abecd146f8e2588db3885c7366ad1dc6
Phil <philbone@focused.cl>
2018-05-16 15:15:41 -0400

    Implementa metodo index de BuyerTransactionController. Agrega la ruta para buyer/transactions.

---
+++ app/Http/Controllers/Buyer/BuyerTransactionController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class BuyerTransactionController extends ApiController
+{
+    /**
+     * Despliega una lista de transacciones de un comprador especifico.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        $transactions = $buyer->transactions;
+
+        return $this->showAll($transactions);
+    }
+}
