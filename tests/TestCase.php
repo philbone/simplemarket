7d9e8b75f3f5c71a5b9d0afd402abde6ef0963ff
Phil <philbone@focused.cl>
2018-05-17 16:20:16 -0400

    Remueve PHP 7.1.0 del build. Modifica seeder para utilizar menos tuplas. Agrega metodo fakeAlfaId() en TestCase para utilizar durante los tests.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,4 +10,24 @@ abstract class TestCase extends BaseTestCase
 
     public const HTTP_OK = 200;
     public const HTTP_NOT_FOUND = 404;
+
+    /**
+    *   Obtiene una cadena de texto aleatoria de largo específico.
+    *   Si el largo no es especificado retornará una cadena de largo 16.
+    *   Si no se especifican los caracterés, se utilizará un $pool alfanúmerico predeterminado.
+    *
+    *   @param  int     $length el largo de la cadena.
+    *   @param  string  $set la lista de caracteres que serán usados para la cadena.
+    *   @return string
+    */
+    public static function fakeAlfaId($length = 16, $set = false)
+    {
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        if ($set != false) {
+            $pool = $set;
+        }
+
+        return substr(str_shuffle(str_repeat($pool, 5)), 0, $length);
+    }
 }
